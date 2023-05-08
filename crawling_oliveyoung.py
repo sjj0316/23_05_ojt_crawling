663bc89d589dbc68a8ade8cfd9001dc2350ea19c
jaejun <sjj050316@gmail.com>
2023-05-08 15:38:45 +0900

    05_08 src-img, name, total fin

--- crawling_oliveyoung.py
+++ crawling_oliveyoung.py
@@ -1,7 +1,24 @@
+#pip install beautifulsoup4
+#pip install requests
+
 import requests
+import bs4
 from bs4 import BeautifulSoup
+
+
 def main():
-    pass
+    url = "https://www.oliveyoung.co.kr/store/main/getHotdealPagingListAjax.do?date=20230508&pageIdx=1&fltCondition=02&fltDispCatNo=&prdSort=rank"
+    data = requests.get(url)
+
+    if data.status_code == 200:
+        soup = bs4.BeautifulSoup(data.text, "html.parser")
+        prodList = soup.find_all("li")
+        for product in prodList:
+            print(product.find("img")["src"])
+            print(product.select_one(".Pnum").text)
+            print(product.select_one(".prod-name").text)
+            print(product.select_one(".total").text)
+            print("--------------------------------")
 
 if __name__ == '__main__':
     main()
